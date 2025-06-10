902c1058049d99ab0de038553a1838570f7b5222
rippleFCL <github@ripple.contact>
2025-06-10 18:47:03 +0100

    fix: strip_exif can take multiple files

--- scripts/strip_exif.sh
+++ scripts/strip_exif.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-file="$1"
+files="$@"
 
+echo "Processing file: $files" >> test.log
 # Check if the orientation tag exists in the file
-if exiftool -orientation "$file" | grep -q 'Orientation'; then
-  # Run the command preserving the orientation tag
-  exiftool -m -all= --icc_profile:all -tagsfromfile @ -orientation -overwrite_original "$file"
-else
-  # Run the command without preserving the orientation tag
-  exiftool -m -all= --icc_profile:all -overwrite_original "$file"
-fi
+for file in $files; do
+    if exiftool -orientation "$file" | grep -q 'Orientation'; then
+        # Run the command preserving the orientation tag
+        exiftool -m -all= --icc_profile:all -tagsfromfile @ -orientation -overwrite_original "$file"
+    else
+        # Run the command without preserving the orientation tag
+        exiftool -m -all= --icc_profile:all -overwrite_original "$file"
+    fi
+    echo "Exif data stripped from $file" >> test.log
+
+done
