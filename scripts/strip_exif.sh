12789626c1aa4016115fa56c6c2c942d0c08d50e
rippleFCL <github@ripple.contact>
2025-06-11 13:34:26 +0100

    feat: preserve EXIF data, and allow parsing sub directories

--- scripts/strip_exif.sh
+++ scripts/strip_exif.sh
@@ -6,8 +6,10 @@ files="$@"
 for file in $files; do
     if exiftool -orientation "$file" | grep -q 'Orientation'; then
         # Run the command preserving the orientation tag
+        echo "Processing $file with orientation tag preserved."
         exiftool -m -all= --icc_profile:all -tagsfromfile @ -orientation -overwrite_original "$file"
     else
+        echo "Processing $file without orientation tag preserved."
         # Run the command without preserving the orientation tag
         exiftool -m -all= --icc_profile:all -overwrite_original "$file"
     fi
